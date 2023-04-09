049edb29b174e96a425ce12bd0a64df9e229c7e7
iceror <ilsecervor@gmail.com>
2023-04-08 19:42:14 -0600

    only numbers in input

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import validator from './validator.js';
 const validateButton = document.getElementById('validate-btn');
 validateButton.addEventListener("click", validateCreditCard);
 
-
 function validateCreditCard() {
   const creditCardNumber = document.getElementById('card-number').value;
-  validator.isValid(creditCardNumber);
-  validator.maskify(creditCardNumber);
-  return (creditCardNumber);
+  if (validateNumbers(creditCardNumber)) {
+    validator.isValid(creditCardNumber);
+    validator.maskify(creditCardNumber);
+    return (creditCardNumber);
+  }
+}
+
+function validateNumbers(creditCardNumber) {
+  const numbers = /^[0-9]+$/;
+  if (creditCardNumber.match(numbers) && creditCardNumber.length === 16) {
+    return true;
+  }
+  else {
+    alert('Ingrese un número de tarjeta válido');
+    //return false;
+  }
 }
