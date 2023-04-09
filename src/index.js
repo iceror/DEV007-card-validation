8f688595d059108e20be89b8d31d266bc6df4208
iceror <ilsecervor@gmail.com>
2023-04-08 19:10:44 -0600

    js code validator && maskify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
 
-console.log(validator);
+const validateButton = document.getElementById('validate-btn');
+validateButton.addEventListener("click", validateCreditCard);
+
+
+function validateCreditCard() {
+  const creditCardNumber = document.getElementById('card-number').value;
+  validator.isValid(creditCardNumber);
+  validator.maskify(creditCardNumber);
+  return (creditCardNumber);
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,37 @@
 const validator = {
-  // ...
+  test: "is working",
+
+  isValid: function (x) {
+    return luhnAlgorithm(x);
+  },
+
+  maskify: function (x) {
+    let hiddenNumbers = x.substring(0, x.length - 4);
+    hiddenNumbers = hiddenNumbers.replace(/\d/g, "#");
+    return hiddenNumbers + x.slice(-4);
+  }
 };
 
+function luhnAlgorithm(x) {
+  let sum = 0;
+  x = (x.split('').reverse().join(''));
+
+  for (let i = 0; i < x.length; i++) {
+    if (i % 2 === 1) {
+      let odd = (x[i] * 2);
+
+      if (odd > 9) {
+        odd = Math.floor(odd / 10) + (odd % 10);
+        // console.log(odd);
+      }
+      sum += Number(odd);
+    } else {
+      sum += Number(x[i]);
+    }
+  }
+  return sum % 10 === 0;
+}
+
 export default validator;
+
+//4083952015263
